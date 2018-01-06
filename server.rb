d6393971f6763cbce1bf1835ec2be4468f24d42c
gbourdon <34389449+gbourdon@users.noreply.github.com>
2018-01-05 21:52:29 -0700

    Upgraded the server's bootup message

--- server.rb
+++ server.rb
@@ -1,5 +1,17 @@
 require_relative 'core.rb'
+PORT = 2000
 
-server = Server.new(2000)
+def initilize
+    puts "Zeus TelChat Server [Version Alpha 0.1.0]"
+    puts
+    server = Server.new(PORT)
+    puts "Server Initilized".upcase
+    puts "IP ADDRESS: #{IPSocket.getaddress(Socket.gethostname)}"
+    puts "PORT: #{PORT}"
+    puts
+    server 
+end
+
+server = initilize
 
 server.run
