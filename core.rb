cdbe75977d12e8d1b7c04f61ef3d2067e3626c68
gbourdon <34389449+gbourdon@users.noreply.github.com>
2018-01-26 22:23:12 -0700

    Removed Vestigial Code

--- core.rb
+++ core.rb
@@ -60,28 +60,3 @@ class Client
         output
     end
 end
-
-class Server
-    attr_reader :port
-
-    def initialize(port = $PORT)
-        @server = TCPServer.open(port)
-        @logs = []
-    end
-
-    def run
-        counter = 1
-        loop do
-            Thread.start(@server.accept) do |client|
-                 message = client.gets.chomp     # Read lines from the socket
-                 unless message == ""
-                     puts "#{counter}|#{message}"
-                     @logs.push "#{counter}|#{message}"
-                     counter += 1
-                 end
-                 client.puts @logs
-                 client.close                  # Disconnect from the client
-            end
-        end
-    end
-end
