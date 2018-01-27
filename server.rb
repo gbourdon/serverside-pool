2e8a2815adcb527ac37c09fc7bca8174f190f2d2
gbourdon <34389449+gbourdon@users.noreply.github.com>
2018-01-26 22:22:42 -0700

    Added the ability to receave messages

--- server.rb
+++ server.rb
@@ -1,17 +1,33 @@
 require_relative 'core.rb'
+require_relative 'link/reciever.rb'
 PORT = 2000
 
 def initilize
-    puts "Zeus TelChat Server [Version Alpha 0.1.0]"
+    puts "Zeus TelChat Server [Version Dev]"
     puts
-    server = Server.new(PORT)
     puts "Server Initilized".upcase
     puts "IP ADDRESS: #{IPSocket.getaddress(Socket.gethostname)}"
     puts "PORT: #{PORT}"
-    puts
-    server 
+    puts 
 end
 
-server = initilize
+initilize
 
-server.run
+server = TCPServer.open(PORT)
+logs = []
+
+loop do
+    Thread.start(server.accept) do |client|  
+        message = client.getMessage
+
+        case message.type
+            when "CHATMSG"
+                unless message.message == ""
+                    puts "#{message.augments['Sender']}:#{message.message}"
+                    logs.push "#{message.augments['Sender']}:#{message.message}"
+                end
+        end
+
+        client.close                  # Disconnect from the client
+    end
+end
