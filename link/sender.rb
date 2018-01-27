b06e4e220dccdbb679a1c48758fe54ae854bcfc4
gbourdon <34389449+gbourdon@users.noreply.github.com>
2018-01-27 13:25:09 -0700

    Ported Sender to a module to avoid conflicts

--- link/sender.rb
+++ link/sender.rb
@@ -2,22 +2,23 @@ require_relative 'encoder.rb'
 require 'socket'
 
 $port = 2000
+module Sender
+    class Client
+        attr_reader :hostname # The name of the computer to connect to
+        attr_reader :port # The specific port to connect with
+        attr_reader :socket
 
-class Client
-    attr_reader :hostname # The name of the computer to connect to
-    attr_reader :port # The specific port to connect with
-    attr_reader :socket
+        def initialize(hostname = 'localhost', port = $port) # Gives the client the info it needs to connect to a server
+            @hostname = hostname
+            @port = port
+            @socket = TCPSocket.open(@hostname, @port)
+        end
 
-    def initialize(hostname = 'localhost', port = $port) # Gives the client the info it needs to connect to a server
-        @hostname = hostname
-        @port = port
-        @socket = TCPSocket.open(@hostname, @port)
-    end
-
-    def sendMessage(type,augments,message = nil) # Sends a message in link format
-        augments = augments.encodeAugments if augments.is_a? Hash
-        @socket.puts type
-        @socket.puts augments
-        @socket.puts message
+        def sendMessage(type,augments,message = nil) # Sends a message in link format
+            augments = augments.encodeAugments if augments.is_a? Hash
+            @socket.puts type
+            @socket.puts augments
+            @socket.puts message
+        end
     end
 end
